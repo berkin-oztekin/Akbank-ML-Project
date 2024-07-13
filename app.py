bd18f97f918837da2a188dd4df1db51cddf94b75
Akin Mert Bur <akinmertbur@gmail.com>
2024-07-14 01:17:54 +0300

    Add microphone functionality and enhance UI

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, request, render_template, jsonify
+import os
+import wave
+import io
+import uuid
 import speech_recognition as sr
 import joblib
 from utils.feature_extraction import extract_features
-from pydub import AudioSegment
-import ffmpeg
 
 app = Flask(__name__)
 
 # Load the trained model
 model = joblib.load('model_training/speaker_recognition_model.pkl')
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/speech-to-text', methods=['POST'])
 def speech_to_text():
@@ -23,35 +27,26 @@ def speech_to_text():
     if audio_file.filename == '':
         return jsonify({'error': 'No file selected for uploading'}), 400
 
-    # Creates an instance of the Recognizer class from the speech_recognition library
     recognizer = sr.Recognizer()
-    # Specifies the path to temporarily save the uploaded audio file
     audio_path = "temp_audio.wav"
-    # Saves the uploaded audio file to the specified path
     audio_file.save(audio_path)
 
-    # Extracts features from the saved audio
     features = extract_features(audio_path)
-    # Uses the pre-trained model to predict the probability that the speaker is the target speaker
     speaker_prob = model.predict_proba([features])[0][1]
 
-    # Adjusted threshold for recognizing the target speaker
-    if speaker_prob < 0.7:  # Change this value to be more strict or lenient
+    if speaker_prob < 0.7:
         return jsonify({'error': 'Speaker not recognized'})
 
-    # Opens the audio file for reading
     with sr.AudioFile(audio_path) as source:
-        # Records the audio from the file
         audio = recognizer.record(source)
         try:
-            # Uses the 'Google Web Speech API' to recognize speech in the audio
             text = recognizer.recognize_google(audio, language="tr-TR")
-            # Returns the recognized text as a JSON response
             return jsonify({'text': text})
         except sr.UnknownValueError:
             return jsonify({'error': 'Speech was unintelligible'})
         except sr.RequestError:
             return jsonify({'error': 'Could not request results from Google Speech Recognition service'})
+
 
 @app.route('/record-speech-to-text', methods=['POST'])
 def record_speech_to_text():
@@ -62,34 +57,33 @@ def record_speech_to_text():
     if audio_file.filename == '':
         return jsonify({'error': 'No file selected for uploading'}), 400
 
-    # Save the uploaded audio file to a temporary path
-    audio_path = "Akbank-ML-Project\\temp_audio"
-    audio_file.save(audio_path)
-   
-    
+    # Read the audio file from the request
+    audio_data = audio_file.read()
+    audio_stream = io.BytesIO(audio_data)
 
-    # # Convert the audio file to WAV format using pydub
-    # wav_path = "temp_audio.wav"
-    # audio = AudioSegment.from_file(audio_path)
-    # audio.export(wav_path, format="wav")
+    # Ensure the file is in WAV format and use it directly
+    try:
+        with wave.open(audio_stream, 'rb') as wf:
+            print(f"Channels: {wf.getnchannels()}, Sample Width: {wf.getsampwidth()}, Frame Rate: {wf.getframerate()}")
+            if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getframerate() not in [16000, 44100, 48000]:
+                return jsonify({'error': 'Unsupported audio format'})
 
-   # Convert the audio file to WAV format using ffmpeg
-    wav_path = "Akbank-ML-Project\\temp_audio.wav"
-    ffmpeg.input(audio_path).output(wav_path).run()
+            audio_path = "temp_audio.wav"
+            with open(audio_path, 'wb') as f:
+                f.write(audio_data)
+    except wave.Error as e:
+        print(f"Wave Error: {e}")
+        return jsonify({'error': 'File is not in WAV format'})
 
-
-    # Initialize the recognizer
     recognizer = sr.Recognizer()
 
-    # Extract features from the WAV audio file
-    features = extract_features(wav_path)
+    features = extract_features(audio_path)
     speaker_prob = model.predict_proba([features])[0][1]
 
-    if speaker_prob < 0.7:  # Change this value to be more strict or lenient
+    if speaker_prob < 0.7:
         return jsonify({'error': 'Speaker not recognized'})
 
-    # Perform speech recognition
-    with sr.AudioFile(wav_path) as source:
+    with sr.AudioFile(audio_path) as source:
         audio = recognizer.record(source)
         try:
             text = recognizer.recognize_google(audio, language="tr-TR")
@@ -98,6 +92,31 @@ def record_speech_to_text():
             return jsonify({'error': 'Speech was unintelligible'})
         except sr.RequestError:
             return jsonify({'error': 'Could not request results from Google Speech Recognition service'})
+
+
+@app.route('/record-target-speaker', methods=['POST'])
+def record_target_speaker():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+
+    audio_file = request.files['file']
+    if audio_file.filename == '':
+        return jsonify({'error': 'No file selected for uploading'}), 400
+
+    target_dir = 'model_training/voice_samples/target_speaker/'
+    os.makedirs(target_dir, exist_ok=True)
+
+    # Generate a random filename
+    random_filename = f"{uuid.uuid4()}.wav"
+    target_path = os.path.join(target_dir, random_filename)
+
+    try:
+        with open(target_path, 'wb') as f:
+            f.write(audio_file.read())
+        return jsonify({'success': True, 'filename': random_filename})
+    except Exception as e:
+        print(f"Error saving target speaker audio: {e}")
+        return jsonify({'error': 'Error saving target speaker audio'})
 
 
 if __name__ == '__main__':
