ac78ac2800c22c67e48e71d5c9cfa4b8fa74d437
Ali İLMAN <aliilman48@gmail.com>
2024-07-14 16:32:15 +0300

    aaa

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ import joblib
 from utils.feature_extraction import extract_features
 from pydub import AudioSegment
 import ffmpeg
+import pvporcupine
+import pyaudio
+import wave
+
 
 app = Flask(__name__)
 
 # Load the trained model
 model = joblib.load('model_training/speaker_recognition_model.pkl')
+
+oturum= False
 
 @app.route('/')
 def index():
@@ -30,13 +36,21 @@ def speech_to_text():
     # Saves the uploaded audio file to the specified path
     audio_file.save(audio_path)
 
+    if oturum == False & isHey_Akbank(audio_path):
+        oturum=True
+    else:
+        return jsonify({'error': 'Oturum daha başlatılmadı'})
+        
+
+
+
     # Extracts features from the saved audio
     features = extract_features(audio_path)
     # Uses the pre-trained model to predict the probability that the speaker is the target speaker
     speaker_prob = model.predict_proba([features])[0][1]
 
     # Adjusted threshold for recognizing the target speaker
-    if speaker_prob < 0.7:  # Change this value to be more strict or lenient
+    if speaker_prob < 0.01:  # Change this value to be more strict or lenient
         return jsonify({'error': 'Speaker not recognized'})
 
     # Opens the audio file for reading
@@ -53,6 +67,27 @@ def speech_to_text():
         except sr.RequestError:
             return jsonify({'error': 'Could not request results from Google Speech Recognition service'})
 
+def isHey_Akbank(audio_path):
+        # Creates an instance of the Recognizer class from the speech_recognition library
+    recognizer = sr.Recognizer()
+       # Opens the audio file for reading
+    with sr.AudioFile(audio_path) as source:
+        # Records the audio from the file
+        audio = recognizer.record(source)
+        try:
+            # Uses the 'Google Web Speech API' to recognize speech in the audio
+            text = recognizer.recognize_google(audio, language="tr-TR")
+            # Returns the recognized text as a JSON response
+            if (text.lower()=="Hey Akbank"):
+                return True
+            else:
+                return False
+        except sr.UnknownValueError:
+            return False
+        except sr.RequestError:
+            return False
+    
+
 @app.route('/record-speech-to-text', methods=['POST'])
 def record_speech_to_text():
     if 'file' not in request.files:
@@ -63,19 +98,18 @@ def record_speech_to_text():
         return jsonify({'error': 'No file selected for uploading'}), 400
 
     # Save the uploaded audio file to a temporary path
-    audio_path = "Akbank-ML-Project\\temp_audio"
+    audio_path = "temp_audio"
     audio_file.save(audio_path)
    
     
+    # Convert the audio file to WAV format using pydub
+    wav_path = "temp_audio.wav"
+    audio = AudioSegment.from_file(audio_path)
+    audio.export(wav_path, format="wav")
 
-    # # Convert the audio file to WAV format using pydub
-    # wav_path = "temp_audio.wav"
-    # audio = AudioSegment.from_file(audio_path)
-    # audio.export(wav_path, format="wav")
-
-   # Convert the audio file to WAV format using ffmpeg
-    wav_path = "Akbank-ML-Project\\temp_audio.wav"
-    ffmpeg.input(audio_path).output(wav_path).run()
+#    # Convert the audio file to WAV format using ffmpeg
+#     wav_path = "temp_audio.wav"
+#     ffmpeg.input(audio_path).output(wav_path).run()
 
 
     # Initialize the recognizer
